58d19c7b0426cff5fb5c50935cfa217d63656b6b
Matt Stump <mrevilgnome@gmail.com>
2014-04-21 16:24:42 -0700

    switched to prepared statements, fixed some math, using the concurrent executor

--- c_insert.py
+++ c_insert.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Queue
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement
+from cassandra.concurrent import execute_concurrent
 import cassandra
 import sys
 import random
@@ -9,21 +10,27 @@ import getopt
 import time
 import datetime
 import collections
-import threading
+
+SEED_NODES = ['alln01-ats-cas1', 'alln01-ats-cas2', 'alln01-ats-cas3', 'alln01-ats-cas4']
+DATACENTER = 'ALLN01'
+KEYSPACE = 'test'
+CONSISTENCY=ConsistencyLevel.LOCAL_ONE
+# SEED_NODES = ['localhost']
+# DATACENTER = None
+# CONSISTENCY=ConsistencyLevel.ONE
 
 def auth_provider(host):
     return {"username" : "cassandra", "password" : "cassandra"}
 
 def connect(seeds, keyspace, datacenter=None, port=9042):
-
-    # from cassandra.io.libevreactor import LibevConnection
+    from cassandra.io.libevreactor import LibevConnection
     from cassandra.cluster import Cluster
     from cassandra.policies import DCAwareRoundRobinPolicy, RetryPolicy, ExponentialReconnectionPolicy
 
     class CustomRetryPolicy(RetryPolicy):
 
         def on_write_timeout(self, query, consistency, write_type,
-                     required_responses, received_responses, retry_num):
+                             required_responses, received_responses, retry_num):
 
             # retry at most 5 times regardless of query type
             if retry_num >= 5:
@@ -42,25 +49,25 @@ def connect(seeds, keyspace, datacenter=None, port=9042):
         load_balancing_policy = DCAwareRoundRobinPolicy(local_dc=datacenter)
 
     cluster = Cluster(contact_points=seeds,
-              port=port,
-              auth_provider=auth_provider,
-              default_retry_policy=CustomRetryPolicy(),
-              reconnection_policy=ExponentialReconnectionPolicy(1, 60),
-              load_balancing_policy=load_balancing_policy)
+                      port=port,
+                      auth_provider=auth_provider,
+                      default_retry_policy=CustomRetryPolicy(),
+                      reconnection_policy=ExponentialReconnectionPolicy(1, 60),
+                      load_balancing_policy=load_balancing_policy)
 
-    # cluster.connection_class = LibevConnection
-    # cluster.set_core_connections_per_host(0, 1)
-    # cluster.set_core_connections_per_host(1, 0)
-    # cluster.control_connection_timeout = 10.0
-    # cluster.set_max_connections_per_host(2, 1)
+    cluster.connection_class = LibevConnection
+    cluster.set_core_connections_per_host(0, 3) # local connections
+    cluster.set_core_connections_per_host(1, 0) # remote connections
+    cluster.control_connection_timeout = 10.0
     cluster.compression = False
-
     session = cluster.connect(keyspace)
     return session
 
-def worker(threadnum, queue):
+def worker(ninserts, threadnum, queue):
     # get connection
-    #connection = connect(['sjm-ats-cas1', 'sjm-ats-cas2', 'sjm-ats-cas3'], keyspace='mfgprod', datacenter='DC1')
+    connection = connect(SEED_NODES, keyspace=KEYSPACE, datacenter=DATACENTER)
+    preparedstmt = connection.prepare("INSERT INTO tst (sernum, area, rectime) VALUES (?, ?, ?)")
+    preparedstmt.consistency_level=CONSISTENCY
 
     inserts = 0
     total_insert_time = 0.0
@@ -70,56 +77,84 @@ def worker(threadnum, queue):
         # make 2 to 9 inserts for this sernum
         ni = random.randint(2,9)
         for i in xrange(ni):
-#             cqlstmt = "INSERT INTO tst (sernum, area, rectime) VALUES ('%s', '%s', '%s') USING CONSISTENCY ONE;" %(sernum, str(i), datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
-             # cqlstmt = SimpleStatement("INSERT INTO tst (sernum, area, rectime) VALUES (%s, %s, %s)", consistency_level=ConsistencyLevel.ONE)
-             start_ins_time = datetime.datetime.now()
-             try:
-                connection.execute(cqlstmt, [sernum, str(i), datetime.datetime.utcnow()])
-             except:
-                print "exception"
+            start_ins_time = datetime.datetime.now()
+            connection.execute(preparedstmt, (sernum, str(i), datetime.datetime.utcnow()))
+            stop_ins_time = datetime.datetime.now()
+            insert_time = (stop_ins_time - start_ins_time).total_seconds()
+            total_insert_time += insert_time
+            inserts += 1
+            if (inserts >= ninserts): break
 
-             stop_ins_time = datetime.datetime.now()
-             insert_time = (stop_ins_time - start_ins_time).total_seconds()
-             total_insert_time += insert_time
-             inserts += 1
-             if (inserts >= ninserts): break
-    print 'Thread %d, performed %d inserts in %f secs' %(threadnum, ninserts, total_insert_time)
+    print 'Thread %d, performed %d inserts in %f secs (%f inserts/sec)' %(threadnum, ninserts, total_insert_time, inserts / total_insert_time)
     connection.shutdown()
     # save all the thread specific data
-    queue.put([total_insert_time])
+    queue.put([total_insert_time, inserts, inserts / total_insert_time])
 
-def main():
-    global nclients, ninserts
+
+def concurrent_worker(ninserts, threadnum, queue):
+    # get connection
+    connection = connect(SEED_NODES, keyspace=KEYSPACE, datacenter=DATACENTER)
+    preparedstmt = connection.prepare("INSERT INTO tst (sernum, area, rectime) VALUES (?, ?, ?)")
+    preparedstmt.consistency_level=CONSISTENCY
+
+    inserts = 0
+    total_insert_time = 0.0
+    while (inserts < ninserts):
+        # make a unique and incremental serial number across threads
+        sernum = 'SN%05X%07X' %(threadnum, inserts)
+        # make 2 to 9 inserts for this sernum
+        statements_and_params = []
+        for i in xrange(random.randint(2,9)):
+            statements_and_params.append([preparedstmt, (sernum, str(i), datetime.datetime.utcnow())])
+
+        start_ins_time = datetime.datetime.now()
+        execute_concurrent(connection, statements_and_params)
+        stop_ins_time = datetime.datetime.now()
+        insert_time = (stop_ins_time - start_ins_time).total_seconds()
+        total_insert_time += insert_time
+        inserts += len(statements_and_params)
+
+    print 'Thread %d, performed %d inserts in %f secs (%f inserts/sec)' %(threadnum, ninserts, total_insert_time, inserts / total_insert_time)
+    connection.shutdown()
+    # save all the thread specific data
+    queue.put([total_insert_time, inserts, inserts / total_insert_time])
+
+def main(nclients, ninserts):
     print "Starting....\n"
     start = datetime.datetime.now()
     threads = []
     thdata = Queue()
-
     for x in xrange(nclients):
-        t = threading.Thread(target=worker, args=(x, thdata))
+        t = Process(target=concurrent_worker, args=(ninserts, x, thdata))
         threads.append(t)
         t.start()
-        time.sleep(0.1)
 
     for thread in threads:
-        thread.join() 
+        thread.join()
+
     end = datetime.datetime.now()
     # gather statistics
     total_insert_time = 0.0
+    total_inserts = 0
+    total_rate = 0
     while not thdata.empty():
-        [insert_time] = thdata.get()
+        [insert_time, inserts, rate] = thdata.get()
         total_insert_time += insert_time
+        total_inserts += inserts
+        total_rate += rate
+
     # because all threads running concurrently
-    total_insert_time = total_insert_time / nclients
+    average_run_time = total_insert_time / nclients
+
     print
-    print 'Average insert time per record (using %d threads %d inserts) is %f millisecs' %(nclients, ninserts, (total_insert_time * 1000)/(nclients * ninserts))
+    print 'Average insert time per record (using %d threads %d inserts) is %f millisecs' % (nclients, ninserts, (total_insert_time * 1000) / total_inserts)
     print
-    print 'No of inserts per second %d' %(nclients * ninserts / total_insert_time)
+    print 'Combined insert rate: %d per second' % (total_rate)
     print
-    print 'Whole test ran for %f minutes' %((end - start).total_seconds()/60)
-    print 
+    print 'Whole test ran for %f seconds' % ((end - start).total_seconds())
+    print
     print "Finished!!\n"
-    
+
 def print_help():
     print '''cassandra_test1.py clients inserts reads
 
@@ -134,18 +169,16 @@ def print_help():
     '''
 
 if __name__ == "__main__":
-    global nclients # num of threads/clients
-    global ninserts # no of inserts
     try:
         options, args = getopt.getopt(
             sys.argv[1:], 'hc:i:', ['clients=',
-                                   'inserts=',
-                                   'help'])
+                                    'inserts=',
+                                    'help'])
     except getopt.GetoptError, err:
         print str(err)
         print_help()
         sys.exit(2)
-        
+
     for opt, arg in options:
         if opt in ('--clients'):
             nclients = int(arg)
@@ -167,10 +200,6 @@ if __name__ == "__main__":
     try:
         ninserts
     except NameError:
-        ninserts = 1000
- 
-    connection = connect(['node0','node1','node2'], keyspace='test', datacenter='us-west')
-    cqlstmt = connection.prepare("INSERT INTO tst (sernum, area, rectime) VALUES (?, ?, ?)")
+        ninserts = 1000000
 
-    main()
-
+    main(nclients, ninserts)
